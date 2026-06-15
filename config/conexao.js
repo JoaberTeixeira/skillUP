3978a2317f2f9734d7733e208af72ffcc7d57ec2
Joaber <joaberdasilva81@gmail.com>
2026-06-15 14:32:50 -0300

    chore: prepare for deployment - add env config and security improvements

--- config/conexao.js
+++ config/conexao.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-const url ="mongodb+srv://joaberdasilva81_db_user:d0fTL2pYK2lqUDRY@cluster0.oylovhj.mongodb.net/?appName=Cluster0"
+
+const url = process.env.MONGODB_URI || "mongodb+srv://joaberdasilva81_db_user:d0fTL2pYK2lqUDRY@cluster0.oylovhj.mongodb.net/?appName=Cluster0"
 
 const conexao = await mongoose.connect(url)
 
